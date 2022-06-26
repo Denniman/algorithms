40a1da928a120917ed5298aa39f1d2f01f310c5c
denniman <dennimanadams@gmail.com>
2022-06-26 22:36:27 +0100

    solve 'breaking camel case' codewars challenge

--- strings/breakCamelCase.js
+++ strings/breakCamelCase.js
@@ -1,24 +1,33 @@
 /**
  * @description Complete the solution so that the function will break up camel casing, using a space between words.
  * @param {string} string
- * @return {string}
+ * @return  string
  */
 
-function solution(string) {
+const solution = (string) => {
   let result = "";
-  if (!string) return result;
+  let lastIndex = 0;
 
-  for (let i = 0; i < string.length; i++) {
-    if (/[A-Z]/.test(string[i])) {
-      result = `${string.substring(0, i)} ${string.substring(i)}`;
-    } else {
-      result += string[i];
+  for (let index = 0; index < string.length; index++) {
+    const char = string[index];
+
+    if (/[A-Z]/.test(char)) {
+      const word = string.substring(lastIndex, index);
+      result += word + " ";
+      lastIndex = index;
+    }
+
+    if (index === string.length - 1) {
+      const word = string.substring(lastIndex, index + 1);
+      result += word;
     }
   }
-  return result;
-}
 
-console.log(solution("camelCasing")); //"camel Casing"
-console.log(solution("identifier")); //""identifier""
-console.log(solution("camelCasingTest")); //
+  return result;
+};
+
+console.log(solution("camelCasing"));
+console.log(solution("identifier"));
+console.log(solution("solutionTestCase"));
 console.log(solution("")); //""
+console.log(solution("dennisTerna"));
