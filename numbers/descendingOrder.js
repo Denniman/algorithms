ab8dc60b58bfd4b66457b3b6de6957684af1b518
denniman <dennimanadams@gmail.com>
2022-06-27 01:40:02 +0100

    fix function signature

--- numbers/descendingOrder.js
+++ numbers/descendingOrder.js
@@ -1,7 +1,7 @@
 /**
  * @description Your task is to make a function that can take any non-negative integer as an argument
  * and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
- * @params {}
+ * @params integer
  * @returns integer
  */
 
