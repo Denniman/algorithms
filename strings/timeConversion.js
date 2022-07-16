62ee8b85e173c0abd2be64325f306b46aec5e2fb
denniman <dennimanadams@gmail.com>
2022-07-16 22:29:38 +0100

    solve 'time conversion' hackerrank challenge

---
+++ strings/timeConversion.js
@@ -0,0 +1,29 @@
+/**
+ * @description Given a time in 12 hours, it converts the time into military 24hours time
+ * @param {s} string
+ * @return string
+ */
+
+function timeConversion(s) {
+  const hourStr = s.substring(0, 2);
+  const timeStr = s.substring(s.length - 2);
+  const minutes = s.substring(3, 5);
+  const seconds = s.substring(6, 8);
+  if (timeStr === "PM") {
+    if (Number(hourStr) > 11) {
+      return `${hourStr}:${minutes}:${seconds}`;
+    }
+    return `${Number(hourStr) + 12}:${minutes}:${seconds}`;
+  }
+
+  if (timeStr === "AM" && Number(hourStr) > 11) {
+    return `00:${minutes}:${seconds}`;
+  }
+
+  return `${hourStr}:${minutes}:${seconds}AM`;
+}
+
+console.log(timeConversion("07:05:45PM"));
+console.log(timeConversion("07:15:45AM"));
+console.log(timeConversion("12:01:00PM"));
+console.log(timeConversion("12:01:00AM"));
