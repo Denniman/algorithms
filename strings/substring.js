ae77e8339b7393479e574e45554dbb1ad1841c39
denniman <dennimanadams@gmail.com>
2022-08-12 23:14:46 +0100

    add substring coderbyte challenge

---
+++ strings/substring.js
@@ -0,0 +1,27 @@
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+/**
+ * @description Given a string s, find the length of the longest substring without repeating characters.
+ * @author Dennis Terna
+ * @param {s} string
+ * @return  integer
+ */
+const lengthOfLongestSubstring = (s) => {
+  const obj = {};
+  let longestStr = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (!obj[s[i]]) {
+      obj[s[i]] = 1;
+      longestStr++;
+    }
+  }
+
+  return longestStr;
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb")); //3
+console.log(lengthOfLongestSubstring("bbbbb")); //1
+console.log(lengthOfLongestSubstring("pwwkew")); //3
