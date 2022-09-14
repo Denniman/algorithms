3d68c5db0e636587c8007fb4f5b62ed3948fb2e5
denniman <dennimanadams@gmail.com>
2022-09-14 21:04:19 +0100

    solve and refactor 'spinal case' fcc problem

--- strings/spinalCase.js
+++ strings/spinalCase.js
@@ -1,12 +1,20 @@
+/**
+ * @description Convert a string to all-lowercase-words-joined-by-dashes.
+ * @param {string} string
+ * @return  string
+ */
+
 function spinalCase(str) {
-  const splitted = str.split("");
+  // This puts a space before any encountered uppercase characters in
+  // the string so that the spaces can be replaced by dashes later on
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
 
-  const result = splitted
-    .map((elem) => elem.match(/[A-Z]/).toLowerCase())
-    .join("");
-  // console.log(splitted.join(""));
-
-  return result;
+  // the regular expression /\s+|_+/g,  select all white spaces and underscores
+  return str.replace(/\s+|_+/g, "-").toLowerCase();
 }
 
-console.log(spinalCase("This Is Spinal Tap"));
+console.log(spinalCase("This Is Spinal Tap")); //this-is-spinal-tap
+console.log(spinalCase("The_Andy_Griffith_Show")); //the-andy-griffith-show
+console.log(spinalCase("Teletubbies say Eh-oh")); //teletubbies-say-eh-oh
+console.log(spinalCase("AllThe-small Things")); //all-the-small-things
+console.log(spinalCase("thisIsSpinalTap")); //all-the-small-things
