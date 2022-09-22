66ebd2f59eed03041c962056cb83f110af2599e3
denniman <dennimanadams@gmail.com>
2022-09-22 20:00:02 +0100

    solve uniteunique fcc medium challenge

---
+++ array/uniteunique.js
@@ -0,0 +1,8 @@
+function uniteUnique(...arr) {
+  const flattenArr = arr.flat();
+  return [...new Set(flattenArr)];
+}
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])); //[1, 3, 2, 5, 4]
+console.log(uniteUnique([1, 2, 3], [5, 2, 1])); //[1, 2, 3, 5]
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])); //[1, 2, 3, 5, 4, 6, 7, 8]
