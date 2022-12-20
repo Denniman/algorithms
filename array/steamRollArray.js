01bae16014de960182f688f06e9f516ac2bb7f72
denniman <dennimanadams@gmail.com>
2022-12-20 10:16:27 +0100

    solve flatten array 'steamrollArray' challenge recursively

---
+++ array/steamRollArray.js
@@ -0,0 +1,13 @@
+
+
+  function steamrollArray(arr) {
+    return arr.reduce(function (flat, toFlatten) {
+      return flat.concat(Array.isArray(toFlatten) ? steamrollArray(toFlatten) : toFlatten);
+    }, []);
+  }
+
+  console.log(steamrollArray([1, [2], [3, [[4]]]]))
+
+  console.log(steamrollArray([1, {}, [3, [[4]]]]))
+
+  
